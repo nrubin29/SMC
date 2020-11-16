1ebca82f1a58d0f1dab97eae8777c22288725e9b
Noah <nrubin29@gmail.com>
2020-11-16 17:06:10 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "SMC",
-    platforms: [
-      .macOS(.v11)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
